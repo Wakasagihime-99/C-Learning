0d8437a626a2c88ad843d1dc40455f8ed7b5dab0
Wakasagihime <fanwenmandy@163.com>
2023-03-02 23:27:55 +0800

    第十章

--- Practice/Practice.c
+++ Practice/Practice.c
@@ -1,38 +1,23 @@
 /*
  * @Date: 2023-02-07
  * @LastEditors: Wakasagihime
- * @LastEditTime: 2023-02-24
+ * @LastEditTime: 2023-03-02
  * @FilePath: \\pythond:\\code\\c\\Practice\\Practice.c
  * @Description: 学习时测试用代码
  */
 
 #include <stdio.h>
 
-void shabi(int a);
 int main()
 {
-    int a = 1;
+    int row, col;
 
-    if (a == 1)
-    {
-        int b = 1;
-    }
-    else
-    {
-        int c = 1;
-    }
+    row = 3;
+    col = 4;
 
-    return 0;
-}
-
-void shabi(int a)
-{
-    int b, c;
-
-    if (a == 1)
-        b = 1;
-    else
-        c = 1;
+    int arr[row][col];
+    
+    int nmsl[2][3] = {{1, 1, 4}, {5, 1, 4}};
 
     return 0;
 }
